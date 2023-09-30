e96843bb69470b72a28b87206daa0f54bec0f791
Christian Smith <csmit232@kent.edu>
2023-09-30 15:17:22 -0700

    update resume

--- src/app/components/contact-me/contact-me.component.ts
+++ src/app/components/contact-me/contact-me.component.ts
@@ -19,6 +19,6 @@ export class ContactMeComponent implements OnInit {
     this.email = "christian.laurienzo@gmail.com";
     this.linkedIn = "https://www.linkedin.com/in/christian-laurienzo-71b43260/";
     this.gitHub = "https://github.com/C-Laurienzo";
-    this.resume = "../../../assets/Resume.pdf"  
+    this.resume = "https://docs.google.com/document/d/1KoK5Jgaot1gLy257eJWsRnJdd-M42Tg7Qpz8Dw0-q_4/edit?usp=sharing"  
   }
 }
---
+++ src/app/components/experience/experience.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'experience',
+  templateUrl: './experience.component.html',
+  styleUrls: ['./experience.component.scss']
+})
+export class ExperienceComponent {
+  public capitalOne: string;
+  public yardi: string;
+  public aws: string;
+  public isShow: boolean;
+
+  constructor() { 
+    this.capitalOne = "https://www.capitalone.com"
+    this.yardi = "https://www.yardi.com/";
+    this.aws = "https://aw.certmetrics.com/amazon/public/verification.aspx"
+    this.isShow = false;
+  }
+}
